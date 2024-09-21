6a6815dcd260701c1cc888b2acc76ff38b0ac723
Vincent D. Warmerdam <vincentwarmerdam@gmail.com>
2024-09-21 14:41:40 +0200

    sorted

--- examples/image.py
+++ examples/image.py
@@ -4,7 +4,7 @@ from rich_pixels import Pixels
 from datatui import datatui
 from pathlib import Path
 
-stream = [{"path": str(p)} for p in Path("examples/pokemons").glob("*.png")]
+stream = [{"path": str(p)} for p in sorted(Path("examples/pokemons").glob("*.png"))]
 
 def render_image(ex):
     with Image.open(ex["path"]) as image:
